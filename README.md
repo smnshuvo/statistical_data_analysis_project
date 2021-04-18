c7c281d19b29d192a3c07689f98c344dabfc229f
smnshuvo <smnshuvo@gmail.com>
2021-04-18 10:51:24 +0600

    Initial Commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Predicting mobile price from related data
+
+## Workflow
+Step 1: As of now I don’t know which data I should keep or remove. I will just go by guessing.
+Step 2: I am seeing that all the data is from the same brand. So I will be removing the brand column, as it seems of no use to me.
+— My analysis: 
+There are
+
+| OS  | Number of devices |
+| ------------- | ------------- |
+| Android  | 915  |
+| EMUI  | 2  |
+| iOS  | 103  |
+| KAiOS  | 1  |
+| OxygenOS  | 3  |
+| WindowsPhone  | 3  |
+| Others  | 197  |
+
+Step 3: Replacing the os data with this numeric value:
+| Name of the OS  | Replaced by |
+| ------------- | ------------- |
+| Android  | 0  |
+| EMUI  | 1  |
+| iOS  | 3  |
+| KAiOS  | 4  |
+| OxygenOS  | 5  |
+| WindowsPhone  | 6  |
+| Others  | 7  |
+Step 4: Still I do see there are some missing values in lowest price and highest price. I am thinking of solving them in the coding environment
+Step 5: Seeing a memory size column where a few featured phones are listed as the memory size doesn't matter there. I will replace those null values with 0s.
+Step 6: Filled the missing screen size from Google search
+The ```final_data.csv``` is the data on which we will be working on.
---
+++ project.R
@@ -0,0 +1,67 @@
+# The codes are written by Shouman Barua Shuvo
+
+#___________________
+#   Read the csv
+#___________________
+
+# Change working directory
+setwd("~/Documents/R_Project")
+# Get the file
+data <- read.csv("final_data.csv")
+data # read successful
+
+
+#___________________
+#     Analyzing
+#___________________
+
+
+# libraries
+library(tidyverse)
+library(ggplot2)
+# install.packages('stringi')
+library(caret)
+library(car)
+library(neuralnet)
+library(Metrics)
+library(magrittr) # needs to be run every time you start R and want to use %>%
+library(dplyr)    # alternatively, this also loads %>%
+# install.packages("GGalley")
+library(GGally)
+
+# removing some columns from data
+data <- data %>%
+  select( -serial, -model_name, -release_date, -lowest_price, -highest_price)
+# I do not think model name and release data has relation
+data
+# plotting
+ggpairs(data)
+
+print(cor(data[,1:7]))
+ggplot(data, aes(x = popularity , y = best_price)) + geom_point()+stat_smooth(method=lm)
+
+corMatrix<-round(cor(data),2)
+corMatrix
+# finding correlation
+findCorrelation(corMatrix, cutoff = 0.7, names = TRUE)
+# regression
+reg<-lm(best_price~os+popularity+sellers_amount+screen_size+memory_size+battery_size,data)
+summary(reg)
+vif(reg) # only screen size is making trouble here
+
+reg<-lm(best_price~os+popularity+sellers_amount+memory_size+battery_size,data)
+summary(reg)
+vif(reg) 
+
+# declare the function
+scale <- function(x){(x - min(x)) / (max(x) - min(x))}
+# mutating the data
+data <- data %>%
+  mutate_all(scale)
+set.seed(12345)
+
+train_data <- sample_frac(data, replace = FALSE, size = 0.80)
+test_data <- anti_join(data, train_data)
+set.seed(12321)
+NN1 <- neuralnet(best_price~os+popularity+sellers_amount+memory_size, data = data)
+plot(NN1, rep = 'best')
